d87ef9d3697123d6ec1b13c44fe1b8dce8df267a
Taylor Kimmett <tkimmett@outlook.com>
2024-11-18 22:44:13 -0500

    use new api to customize storage account

--- Lenderboxd.AppHost/Program.cs
+++ Lenderboxd.AppHost/Program.cs
@@ -1,9 +1,16 @@
-﻿using Azure.ResourceManager.Storage.Models;
+﻿using Azure.Provisioning.Storage;
 using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var azStorage = builder.AddAzureStorage("storage", StorageKind.Storage, StorageSkuName.StandardLrs).RunAsEmulator();
+// var azStorage = builder.AddAzureStorage("storage", StorageKind.Storage, StorageSkuName.StandardLrs).RunAsEmulator();
+var azStorage = builder.AddAzureStorage("storage").ConfigureInfrastructure(c =>
+{
+	var account = c.GetProvisionableResources().OfType<StorageAccount>().Single();
+	account.Kind = StorageKind.Storage;
+	account.Sku = new() { Name = StorageSkuName.StandardLrs };
+}).RunAsEmulator();
+
 var tables = azStorage.AddTables("tables");
 var queues = azStorage.AddQueues("queues");
 var orleans = builder.AddOrleans("lenderboxd-cluster")
